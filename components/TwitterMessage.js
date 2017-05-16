b867be25313c58cf4a418fe39a488794a42d6003
Vinny Martinez <vinny@vinnymartinez.com>
2017-05-15 23:50:52 -0400

    got tweet message working

--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -1,17 +1,31 @@
 const React = require('react');
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      tweet: '',
+      maxChars: this.props.maxChars
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+
+    this.setState({
+      tweet: event.target.value,
+      maxChars: this.state.maxChars - 1
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.tweet} onChange={this.handleChange} />
+        <p>Max chars: {this.state.maxChars}</p>
       </div>
     );
   }
